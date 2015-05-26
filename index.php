c4727ded37d29720c2a6d324f0b8b4f7e95e9e89
RICHARD HAMILTON <richardgregoryhamilton@gmail.com>
2015-05-26 14:31:42 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+  
+  <head>
+    <link rel="stylesheet" href="stylesheets/styles.css"></link>
+  </head>
+  <body>
+
+  <form id="search-form">
+    Enter keywords:    
+    <input id="keyword" type="text" value="Bach">
+    <button onclick="search()">Search...</button>
+  </form>
+	
+	<div id="output"></div>
+      
+	<script type="text/javascript" src="scripts/main.js"></script>
+  
+	</body>
+</html>
